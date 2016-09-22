c53d5a493b9d5b18c55ef84444b8b2b1e7f30d22
Stefan Schonert <sh@schonert.dk>
2016-09-22 15:01:06 +0200

    clean up + match breaking changes in sky-list

--- sky-search.directive.ts
+++ sky-search.directive.ts
@@ -1,54 +1,41 @@
 (function() {
 	'use strict';
-	
-	angular.module('skySearch').directive('skySearch',skySearchDirective);	
-	
-	skySearchDirective.$inject = ['skyList','skyQueryString'];
-	
-	function skySearchDirective(skyList:sky.ISkyListFactory, skyQueryString) {
-		var directive = {
+
+	angular.module('skySearch').directive('skySearch',skySearchDirective);
+
+	skySearchDirective.$inject = ['$rootScope', 'skyList','skyQueryString'];
+
+	function skySearchDirective($rootScope, skyList, skyQueryString) {
+		return {
 			restrict:'E',
 			templateUrl:'/sky-search/sky-search.template.html',
 			scope:{},
 			controller:controller,
 			controllerAs:'skySearchCtrl'
 		};
-		
-		controller.$inject = ['$scope'];
-		function controller($scope) {
-			var _this = this;
-			
-			_this.query = {
-				keywords:''
+
+		function controller() {
+			let configuration = {
+				api: '/umbraco/api/sitesearchapi/search',
+				limit:5
 			};
-			
-			var lastQuery = _this.query.keywords;
-			
-			var keywords = skyQueryString.get('keywords');
-			if(keywords) {
-				_this.query.keywords = keywords;
-			}
 
-			skyList.createInstance('search',{ api: '/umbraco/api/sitesearchapi/search', limit:5 }).then(function(searchList) {
-				_this.data = searchList.results;
-				_this.showMore = searchList.getNext;
-				
-				$scope.$watch(()=>_this.query.keywords, function() {
-					if(_this.query.keywords.length<2) {
-						return searchList.empty();
-					}
-					if(lastQuery != _this.query.keywords) {
-						searchList.getResults(_this.query);
-					}
-					lastQuery = _this.query.keywords;
+			configuration.api = '/umbraco/api/PhonebookApi/Search/';
+
+			skyList.createInstance('search',configuration).then((list) => {
+				this.query = list.query;
+				this.data = list.results;
+				this.showMore = list.nextPage;
+
+				// Merge query from location.search
+				angular.merge(this.query, skyQueryString.getAll());
+
+				// Get results on query change
+				$rootScope.$watchCollection(()=> this.query, () => {
+					list.getResults();
 				});
-				
 			});
-
 		}
-		
-		return directive;
-		
 	}
-	
+
 })();
